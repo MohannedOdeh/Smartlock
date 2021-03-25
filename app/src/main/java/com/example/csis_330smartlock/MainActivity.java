55e952dd61e57e7b8d315213643377389b3fa111
ragequitting101 <davidliang104@gmail.com>
2021-03-25 18:53:20 +0300

    :)

--- app/src/main/java/com/example/csis_330smartlock/MainActivity.java
+++ app/src/main/java/com/example/csis_330smartlock/MainActivity.java
@@ -11,6 +11,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity {
+    //:) :) :) :) :) :) :) :) :) :) :) :)
     EditText firstName;
     EditText lastName;
 
