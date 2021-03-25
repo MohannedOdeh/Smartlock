1e7a6b5fd750c36aba5319f6a3f912ba6ed67dea
ragequitting101 <davidliang104@gmail.com>
2021-03-25 19:20:51 +0300

    Logout in MainActivity

--- app/src/main/java/com/example/csis_330smartlock/MainActivity.java
+++ app/src/main/java/com/example/csis_330smartlock/MainActivity.java
@@ -2,24 +2,27 @@ package com.example.csis_330smartlock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class MainActivity extends AppCompatActivity {
-    //:( :( :( :( :( :( :(
-    //Hi David
     EditText firstName;
     EditText lastName;
-
     EditText password;
     EditText email;
     Button btnSignUp;
+    Button logout;
+    TextView welcome;
     private FirebaseAuth mAuth;
+
     private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +30,23 @@ public class MainActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         setContentView(R.layout.activity_main);
 
+        logout = findViewById(R.id.btnLogout);
+        welcome = findViewById(R.id.welcome);
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            welcome.setText("Welcome " + currentUser.getDisplayName());
+        }
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
